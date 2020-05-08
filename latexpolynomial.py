b1f6f8c9cdad3c960f0fa573aa5a161851b5f008
khssupriya <khssupriya@gmail.com>
2020-05-08 17:44:43 +0530

    adding latex polynomial

---
+++ latexpolynomial.py
@@ -0,0 +1,19 @@
+def pre_process(polynomial):
+    return polynomial.strip("$").replace("+ ","+").replace("- ","-")
+
+def mathematician_latex_polynomial(polynomial: str)->str:
+    polynomial = pre_process(polynomial)
+
+    if polynomial[0] != "-":
+        polynomial = "+" + polynomial
+
+    expressions = polynomial.split(" ")
+    mathematician_polynomial=" ".join(expressions[::-1])
+
+    if mathematician_polynomial[0] == "+":
+        mathematician_polynomial = mathematician_polynomial[1:]
+
+    return  "$" + mathematician_polynomial[:-1] + "$"
+
+print(mathematician_latex_polynomial("$3x^4 -7x^3 +21x +17$"))
+print(mathematician_latex_polynomial("$3x^4 - 7x^3 + 21x +17$"))
